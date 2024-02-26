d79842736c7701d2d2834eb4310f80f5007720c9
Arjun Pawar <108388620+Arjun0023@users.noreply.github.com>
2024-02-27 01:29:17 +0530

    messed up backend

--- backend/db.js
+++ backend/db.js
@@ -37,10 +37,21 @@ const userSchema = new mongoose.Schema({
         maxLenght:20
     }
 })
-
+const accountSchema = new mongoose.Schema({
+    userId: {
+        type: mongoose.Schema.Types.ObjectId, // Reference to User model
+        ref: 'User',
+        required: true
+    },
+    balance: {
+        type: Number,
+        required: true
+    }
+});
+const Account = mongoose.model('Account', accountSchema);
 //Create a model from the Schema
 const User = mongoose.model('User',userSchema);
 
 module.exports = {
-    User
+    User,Account
 };

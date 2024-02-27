bdaa98b1c61bdd576f8e29005c68d85746134bef
Arjun Pawar <108388620+Arjun0023@users.noreply.github.com>
2024-02-27 16:04:17 +0530

    frontend

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,18 +1,24 @@
-import { useState } from "react"
-import './App.css'
+import {
+  BrowserRouter,
+  Route,
+  Routes,
+} from "react-router-dom";
+import { Signup } from "./pages/Signup";
+import { Signin } from "./pages/Signin";
+import { Dashboard } from "./pages/Dashboard";
+import { SendMoney } from "./pages/SendMoney";
 
 function App() {
-
   return (
     <div>
     <BrowserRouter>
-      <Routes>
-        <Route path="/signup" element={<Signup/>}/>
-        <Route path="/signin" element={<Signin/>}/>
-        <Route path="/dashboard" element={<Dashboard />} />
-        <Route path="/send" element={<SendMoney />} />
+    <Routes>
+      <Route path="/signup" element={<Signup />} />
+      <Route path="/signin" element={<Signin />} />
+      <Route path="/dashboard" element={<Dashboard />} />
+      <Route path="/send" element={<SendMoney />} />
       </Routes>
-    </BrowserRouter>
+      </BrowserRouter>
     </div>
   )
 }

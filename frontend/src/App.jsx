41f5727e0666036f1ffcc7ca6153df0db16b7df0
Arjun Pawar <108388620+Arjun0023@users.noreply.github.com>
2024-02-27 15:42:10 +0530

    frontend hard code

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -5,7 +5,14 @@ function App() {
 
   return (
     <div>
-    Hello world
+    <BrowserRouter>
+      <Routes>
+        <Route path="/signup" element={<Signup/>}/>
+        <Route path="/signin" element={<Signin/>}/>
+        <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/send" element={<SendMoney />} />
+      </Routes>
+    </BrowserRouter>
     </div>
   )
 }
